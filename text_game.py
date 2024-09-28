7b2422530b9ec79ddbdfc657bb1430a3012b6fe0
bharath <bharathmuruga9337@gmail.com>
2024-09-28 12:49:10 +0530

    text_based_game

---
+++ text_game.py
@@ -0,0 +1,28 @@
+print("welcome to our game")
+name=input("enter the your name: ")
+print(f"hello {name} welcome")
+
+user_input=input("do you want to play (yes/no): ").lower()
+if user_input=="yes":
+    print("you are  at center of forset")
+
+    direction=input("enter which direction do you want to go(right/left): ").lower()
+    if direction=="right":
+        print("you see river")
+        choice=input("do you want swim or use boat(swim/boat): ")
+        if choice=="swim":
+          print("eaten by alligator ,you died, try again")
+        else:
+           print("found gold , won the game")
+     
+    elif direction=="left":
+       print("you are eaten by a lion ")
+    
+
+    
+    
+else:
+    print("you are exiting the game")
+
+
+

27ffc8e7776b45b3f6f8cf16678829048a9ca79a
bharath <bharathmuruga9337@gmail.com>
2024-09-29 12:22:30 +0530

    add, fix

--- calculator.py
+++ calculator.py
@@ -1,20 +1,43 @@
-num1=float(input("enter the number1: "))
-num2=float(input("enter the number2: "))
-result=0
-sign=input("enter the sign: ")
+def get_number():
+    while True:
+      try:
+        num=input("enter the number: ")
+        num=float(num)
+        break 
+      except:
+        print("invalid number try ,again")
+    return num
+  
+def get_sign():
+  while True:
+    sign=input("enter the sign: ")
+    if sign=="+" or sign=="-" or sign=="*" or sign=="/":
+      return sign
+    else:
+      print("invalid sign")
 
-if sign=="+":
-  result=num1+num2
-elif sign=="-":
-  result=num1-num2
-elif sign=="*":
-  result=num1*num2
-elif sign=="/":
-  if num2!=0:
-    result=num1/num2
-  else:
-    print("zero division")
-else:
-  print("invalid sign")
 
-print(result)
+def main():
+  num1=get_number()
+  num2=get_number()
+  sign=get_sign()
+ 
+  result=0
+  if sign=="+":
+      result=num1+num2
+  elif sign=="-":
+      result=num1-num2
+  elif sign=="*":
+          result=num1*num2
+  elif sign=="/":
+        if num2!=0:
+          result=num1/num2
+        else:
+          print("zero division")
+
+
+  print(result)
+
+if __name__=="__main__":
+  main()
+

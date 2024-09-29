aca483b803d7efa41aecf445a87da50c3a9e39e1
bharath <bharathmuruga9337@gmail.com>
2024-09-29 20:31:05 +0530

    change,add

--- contact_management.py
+++ contact_management.py
@@ -1,12 +1,18 @@
 
-def add_person():
+def display(people):
+  for i ,person, in enumerate(people):
+    print(i+1,"-",person["name"],"|",person["age"],"|",person["email"])
 
-  name=input("enter the name: ")
+
+def add_person():
+  name=input("enter the name: ").lower()
   while True:
     try:
       age=int(input("enter the age: "))
-
-      break
+      if age<=0 or age>=110:
+        print("invalid age")
+      else:
+        break
 
     except :
       print("invaild age ")
@@ -14,26 +20,63 @@ def add_person():
   email=input("enter the email: ")
   person={"name":name,"age":age,"email":email}
   return person
-def search_person():
-  pass
-def delete_person():
-  pass
+
+
+def search_person(people):
+  search_name=input("enter the name: ").lower()
+  result=[person for person in people if search_name in person["name"]]
+  if result:
+    display(result)
+  else:
+    print("not found")
+
+def total_contact(people):
+  print(f"number of contact: {len(people)}")
+
+  
+def delete_person(people):
+  if not people:
+    print("contact is empty!!!")
+    return
+  display(people)
+  while True:
+    try:
+        dele=int(input("enter the number :"))
+        if dele!=0 and dele<=len(people):
+          people.pop(dele-1)
+          print(" deleted successfully!!!")
+          break
+        else:
+          print("number does not exist!!!")
+    except:
+        print("invalid input!!!")
+
   
 print("HI WELCOME TO CONTACT MANAGEMENT LIST")
 print()
 people=[]
 
-command=input("You can 'ADD' ,'SEARCH','DELETE', person: ").lower()
-if command=="add":
-  person=add_person()
-  persons.append(person)
-elif command=="search":
-  pass
-elif command=="delete":
-  pass
-else:
-  print("invalid input!!!")
 
 
+def main():
+  while True:
+    command=input("You can 'ADD' ,'SEARCH','DELETE' and 'Q' for quit: ").lower()
+    if command=="add":
+      person=add_person()
+      people.append(person)
+      print("person add successfully")
+      total_contact(people)
+    elif command=="search":
+      search_person(people)
+    elif command=="delete":
+      delete_person(people)
+      total_contact(people)
+    elif command=="q":
+      print("your exiting the application!!!")
+      break
+    else:
+      print("invalid input!!!")
 
-print(persons)
+
+if __name__=="__main__":
+   main()
